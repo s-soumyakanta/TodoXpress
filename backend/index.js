118f496a600694236123d82024be5f79b3d6627d
S Soumyakanta <ssoumyakanta@fightright.in>
2024-03-21 08:54:50 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+const app = express();
+const routes = require("./src/routes/TodoRoutes")
+const PORT = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DB Connected..."))
+  .catch((err) => console.log("Error in Mongoose Connect, " + err.message));
+
+
+app.use("/api",routes)
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
