8b6cd3f9df0dfea4ad452f20981532d5a995507d
S Soumyakanta <92138792+s-soumyakanta@users.noreply.github.com>
2024-03-22 02:44:36 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const proxyUrl = import.meta.env.VITE_PROXY_URL || 'http://localhost:5000';
+
 export default defineConfig({
   server: {
     proxy: {
-      "/api": import.meta.env.VITE_PROXY_URL
+      "/api": proxyUrl
     }
   },
   plugins: [react()]
